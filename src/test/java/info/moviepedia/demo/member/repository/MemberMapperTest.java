7ab4eda14cdea84bd7b6dc1caa6328b7129be92a
Inno Song <songinno@naver.com>
2022-02-14 12:23:53 +0900

    TEST 주석처리

--- src/test/java/info/moviepedia/demo/member/repository/MemberMapperTest.java
+++ src/test/java/info/moviepedia/demo/member/repository/MemberMapperTest.java
@@ -8,43 +8,43 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 
-@SpringBootTest
-class MemberMapperTest {
-
-    @Autowired
-    MemberMapper memberMapper;
-
-    @Test
-    void regTest() {
-        Member member = new Member();
-        member.setMemberEmail("aaaa1651615@naver.com");
-        member.setMemberName("손오공");
-        member.setMemberPassword("ssss123");
-
-        memberMapper.register(member);
-    }
-
-    @Test
-    void deleteTest() {
-        memberMapper.delete("abc123@naver.com");
-    }
-
-    @Test
-    void getOneTest() {
-        Member member = memberMapper.getOneMember("songinno@naver.com");
-
-        System.out.println("============================");
-        System.out.println("member = " + member);
-        System.out.println("============================");
-    }
-
-    @Test
-    void getListTest() {
-        List<Member> memberList = memberMapper.getMemberList();
-
-        for (Member member : memberList) {
-            System.out.println("member = " + member);
-        }
-    }
-
-}
+//@SpringBootTest
+//class MemberMapperTest {
+//
+//    @Autowired
+//    MemberMapper memberMapper;
+//
+//    @Test
+//    void regTest() {
+//        Member member = new Member();
+//        member.setMemberEmail("aaaa1651615@naver.com");
+//        member.setMemberName("손오공");
+//        member.setMemberPassword("ssss123");
+//
+//        memberMapper.register(member);
+//    }
+//
+//    @Test
+//    void deleteTest() {
+//        memberMapper.delete("abc123@naver.com");
+//    }
+//
+//    @Test
+//    void getOneTest() {
+//        Member member = memberMapper.getOneMember("songinno@naver.com");
+//
+//        System.out.println("============================");
+//        System.out.println("member = " + member);
+//        System.out.println("============================");
+//    }
+//
+//    @Test
+//    void getListTest() {
+//        List<Member> memberList = memberMapper.getMemberList();
+//
+//        for (Member member : memberList) {
+//            System.out.println("member = " + member);
+//        }
+//    }
+//
+//}
